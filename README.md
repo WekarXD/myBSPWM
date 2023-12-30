55d2e4d6c9b5f166c79b87e8e3f8649ef2affdde
Wekar <pablo.garcialeon@gmail.com>
2023-12-30 12:48:25 +0100

    Creacion de archivos de configuracion de bspwm y sxhkd

--- README.md
+++ README.md
@@ -12,6 +12,20 @@ Puedes actualizarlo con el siguiente comando
 sudo apt update && sudo parrot-upgrade -y
 ```
 
+> [!NOTE]
+> Actualmtente en parrot os hay un error a la hora de hacer un upgrade nos da un error de kernel.
+> Para solucionar este error hay que borrar un paquete antes de hacer el upgrade.
+> Esta seria una manera de actualizar
+> ´´´
+> sudo apt update 
+> sudo apt remove realtek-rtl8188eus-dkms
+> sudo apt autoclean && sudo apt autoremove
+> parrot-upgrade
+> sudo parrot-upgrade
+> sudo apt autoremove 
+> reboot
+> ´´´
+
 ## 1.- Instalación
 
 ```
--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 # Funcion instalar paquetes necesarios
 install_packages() {
 
-    PAKAGE_MANAGER="apt-get"
+    PAKAGE_MANAGER="apt"
 
 
     #Actualiazar paquetes
-    if [ "$PAKAGE_MANAGER" == "apt-get" ]; then
+    if [ "$PAKAGE_MANAGER" == "apt" ]; then
         sudo $PAKAGE_MANAGER update
     fi
 
@@ -17,18 +17,20 @@ install_packages() {
 
     echo "Buscando Paquetes necesarios..."
 
-    PAKAGE_COMMON="libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev g++ clang git cmake pkg-config python3 python3-sphinx python3-packaging"
+    PAKAGE_COMMON="kitty libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev g++ clang git cmake pkg-config python3 python3-sphinx python3-packaging cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev"
 
     for package in $PAKAGE_COMMON; do
         if [ "$PAKAGE_MANAGER" == "apt" ] && ! dkpg -l | grep -q " $package "; then
             echo "Instalando $package..."
             sudo $PAKAGE_MANAGER install -y $package
-            echo "Se ha instalado $package"
+            echo -e "[+] Se ha instalado $package"
             sleep 2
         else
-            echo "$package ya está instalado."
+            echo "[!] $package ya está instalado."
+            sleep 5
         fi
     done
+    pip install -U sphinx
 }
 
 
@@ -51,12 +53,14 @@ sleep 1.5
 cd ~/github/bspwm
 make -j$(nproc)
 sudo make install
+sleep 10
 # instalando sxhkd
 echo "Instalando sxhkd..."
 sleep 1.5
 cd ~/github/sxhkd
 make -j$(nproc)
 sudo make install
+sleep 10
 # Instalando Polybar
 echo "Instalando polybar..."
 sleep 1.5
@@ -66,6 +70,7 @@ cd build
 cmake ..
 make -j$(nproc)
 sudo make install
+sleep 10
 # Instalando Picom
 # echo "Instalando picom..."
 # sleep 1.5
@@ -74,3 +79,8 @@ sudo make install
 # meson --buildtype=release . build
 # ninja -C build
 # sudo ninja -C build install
+
+# Copiando Archivos de Configuración
+echo "Configurando..."
+sleep 1.5
+cp -rv $ruta/Config/* ~/.config/
