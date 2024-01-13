caf03d5d72c34f11d08c6bd711252d905ebcb28d
Wekar <pablo.garcialeon@gmail.com>
2024-01-13 21:12:52 +0100

    COLORS

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# COLORS
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+WHITE="\e[97m"
+ENDCOLOR="\e[0m"
+
+
 # Funcion instalar paquetes necesarios
 install_packages() {
 
@@ -16,18 +27,18 @@ install_packages() {
 
     # Comando para instalar los paquetes necesarios
 
-    echo "Buscando Paquetes necesarios..."
+    echo "${BLUE}Buscando Paquetes necesarios...${ENDCOLOR}"
 
     PAKAGE_COMMON=" rofi zsh zsh-autosuggestions zsh-syntax-highlighting zsh-autocomplete npm flameshot ranger imagemagick feh locate libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev build-essential git cmake cmake-data pkg-config python3-packaging libcairo2-dev libxcb1-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev g++ clang python3 libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libpcre2-dev libpcre3 libpcre3-dev libpixman-1-dev libx11-xcb-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-present-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev libjs-sphinxdoc=5.3.0-4 python3-sphinx"
 
     for package in $PAKAGE_COMMON; do
         if [ "$PAKAGE_MANAGER" == "apt" ] && ! dpkg -l | grep -q " $package "; then
-            echo "Instalando $package..."
+            echo -e "${BLUE}Instalando ${PURPLE}$package...${ENDCOLOR}"
             sudo $PAKAGE_MANAGER install -y --allow-downgrades $package
-            echo -e "[+] Se ha instalado $package"
+            echo -e "${GEERN} [+] Se ha instalado $package${ENDCOLOR}"
             sleep 1
         else
-            echo "[!] $package ya está instalado."
+            echo -e "${RED}[!] $package ya está instalado.${ENDCOLOR}"
         fi
     done
 }
@@ -39,7 +50,7 @@ install_packages
 
 mkdir ~/github
 cd ~/github
-echo "Instalando REPOSITORIOS..."
+echo -e "${BLUE}Instalando Repositorios...${ENDCOLOR}"
 sleep 1.5
 git clone https://github.com/baskerville/bspwm.git
 git clone https://github.com/baskerville/sxhkd.git
@@ -60,22 +71,24 @@ wget https://github.com/kovidgoyal/kitty/releases/latest/download/kitty-$kitty_l
 kitty_bin=$(ls | grep "kitty")
 
 #instalando bspwm
-echo "Instalando bspwm..."
+echo -en "${BLUE}Instalando bspwm...    ${ENDCOLOR}"
 sleep 1.5
 cd ~/github/bspwm
 make -j$(nproc)
 sudo make install
 sudo apt-get install bspwm -y
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 # instalando sxhkd
-echo "Instalando sxhkd..."
+echo -en "${BLUE}Instalando sxhkd...    ${ENDCOLOR}"
 sleep 1.5
 cd ~/github/sxhkd
 make -j$(nproc)
 sudo make install
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 # Instalando Polybar
-echo "Instalando polybar..."
+echo -en "${BLUE}Instalando polybar...    ${ENDCOLOR}"
 sleep 1.5
 cd ~/github/polybar
 mkdir build
@@ -83,18 +96,20 @@ cd build
 cmake ..
 make -j$(nproc)
 sudo make install
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 # Instalando Picom
-echo "Instalando picom..."
+echo -en "${BLUE}Instalando picom...    ${ENDCOLOR}"
 sleep 1.5
 cd ~/github/picom
 git submodule update --init --recursive
 meson --buildtype=release . build
 ninja -C build
 sudo ninja -C build install
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 # Instalando la kitty
-echo "Instalando kitty..."
+echo -en "${BLUE}Instalando kitty...${ENDCOLOR}"
 sleep 1.5
 cd ~/github
 sudo mkdir /opt/kitty
@@ -105,54 +120,64 @@ sudo rm $kitty_bin
 kitty_bin=$(ls | grep "kitty")
 sudo tar -xf $kitty_bin
 sudo rm $kitty_bin
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 #Instalando bat
+echo -en "${BLUE}Instalando Batcat...    ${ENDCOLOR}"
 cd ~/github
 sudo dpkg -i $bat_bin
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 #Instalando lsd
+echo -en "${BLUE}Instalando LSD...    ${ENDCOLOR}"
 cd ~/github
 sudo dpkg -i $lsd_bin
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 # Instalando p10k
-echo "Instalando P10K..."
+echo -en "${BLUE}Instalando P10K...    ${ENDCOLOR}"
 sleep 1.5
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
 echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 # Instalando p10k root
 sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/.powerlevel10k
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 # Instalando las HackNerdFonts
-echo "Instalando las HNF"
+echo -en "${BLUE}Instalando las Fuentes...    ${ENDCOLOR}"
 sleep 1.5
 sudo cp -v $ruta/fonts/HNF/* /usr/local/share/fonts/
-
 # Instalando las fuentes en Polybar
 sudo cp -v $ruta/Config/polybar/fonts/* /usr/share/fonts/truetype/
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 # Copiando Archivos de Configuración
-echo "Configurando..."
+echo -en "${BLUE}Copiando archivos de configuracion...    ${ENDCOLOR}"
 sleep 1.5
 cp -rv $ruta/Config/* ~/.config/
 sudo cp -rv $ruta/Config/kitty ~/.config/
 # Kitty Root
 sudo cp -rv $ruta/Config/kitty /root/.config/
-
 # Copia de configuracion de .p10k.zsh y .zshrc
 rm -rf ~/.zshrc
 cp -v $ruta/zshrc ~/.zshrc
 cp -v $ruta/p10k.zsh ~/.p10k.zsh
 sudo cp -v $ruta/p10k.zsh-root /root/.p10k.zsh
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 # Plugins ZSH
+echo -en "${BLUE}Instalando plugins ZSH...    ${ENDCOLOR}"
 sudo mkdir /usr/share/zsh-sudo
 cd /usr/share/zsh-sudo
 sudo wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 # Cambiando de SHELL a ZSH
+echo -en "${BLUE}${ENDCOLOR}"
 chsh -s /usr/bin/zsh
 sudo usermod --shell /usr/bin/zsh root
 sudo ln -s -fv ~/.zshrc /root/.zshrc
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 # Asignando permisos
 chmod u+x ~/.config/bspwm/bspwmrc
@@ -161,30 +186,32 @@ chmod +x ~/.config/bin/htb_status.sh
 chmod +x ~/.config/bin/htb_target.sh
 
 # Instalando Nvim + Nvchad
-sudo apt remove neovim
-rm -rf ~/.config/nvim
-rm -rf ~/.local/share/nvim
+echo -en "${BLUE}Instalando Nvchad...    ${ENDCOLOR}"
+sudo apt remove neovim -y
+sudo rm -rf ~/.config/nvim
+sudo rm -rf ~/.local/share/nvim
 cd /opt/
 sudo wget -P /opt/ https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo tar xzvf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
+rm -f nvim-linux64.tar.gz
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 sudo cp -r ~/.config/nvim /root/.config
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 # Install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+~/.fzf/install --all
 
-sudo git clone --depth 1 https://github.com/junegunn/fzf.git root/.fzf
-sudo /root/.fzf/install
+sudo git clone --depth 1 https://github.com/junegunn/fzf.git /root/.fzf
+sudo /root/.fzf/install --all
 
-# Configuramos el Tema de Rofi
 # Configuramos el tema Nord de Rofi:
-echo "Instalando Rofi..."
+echo -en "${BLUE}Instalando Rofi...    ${ENDCOLOR}"
 sleep 1.5
 mkdir -p ~/.config/rofi/themes
 cp $ruta/rofi/nord.rasi ~/.config/rofi/themes/
 rofi-theme-selector
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 # Instalando Wallpapers
 sleep 1.5
@@ -194,24 +221,26 @@ echo "# WALLPAPER" >> ~/.config/bspwm/bspwmrc
 echo "feh --bg-fill ~/Wallpaper/death.jpg &" >> ~/.config/bspwm/bspwmrc
 
 # Eliminando restos
+echo -en "${BLUE}Limpiando restos...    ${ENDCOLOR}"
 rm -rf ~/github
 rm -rf $ruta
+echo -e "${GREEN}Listo.${ENDCOLOR}"
 
 # Mensaje de instalado
 notify-send -e "BSPWM Instalado
 Hay que reiniciar y cambiar el entorno a bspwm"
 
 while true; do
-    echo -en "[?] Es necesario reiniciar el sistema. ¿Deseas reiniciar ahora?"
+    echo -en "${PURPLE}[?] Es necesario reiniciar el sistema. ¿Deseas reiniciar ahora?${ENDCOLOR}"
     read -r
     REPLY=${REPLY:-"y"}
     if [[ $REPLY =~ ^[YySs]$ ]]; then
-        echo -e "[+] Reiniciando el sistema..."
+        echo -e "${GREEN}[+] Reiniciando el sistema...${ENDCOLOR}"
         sleep -1
         sudo reboot
     elif [[ $REPLY =~ ^[Nn]$ ]]; then
         exit 0
     else
-        echo -e "[!] Respuesta invalida, responda otra vez."
+        echo -e "${RED}[!] Respuesta invalida, responda otra vez.${ENDCOLOR}"
     fi
 done
