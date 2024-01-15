12519b96447b2b02682b5e2d2a509610ecff4610
Wekar <pablo.garcialeon@gmail.com>
2024-01-15 20:40:38 +0100

    Silent

--- install.sh
+++ install.sh
@@ -33,6 +33,7 @@ install_packages() {
 
     for package in $PAKAGE_COMMON; do
         if [ "$PAKAGE_MANAGER" == "apt-get" ] && ! dpkg -l | grep -q " $package "; then
+            sudo ls &>/dev/null
             echo -e "${BLUE}[ ] Instalando ${MAGENTA}$package...${ENDCOLOR}"
             sudo $PAKAGE_MANAGER install -yqq --allow-downgrades $package 2>/dev/null >/dev/null
             echo -e "${GEERN}[+] Se ha instalado $package${ENDCOLOR}"
