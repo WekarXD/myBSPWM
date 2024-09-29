a104b89ab8053a6b75acaead91d96146b04a8faf
Wekar <pablo.garcialeon@gmail.com>
2024-09-30 01:42:45 +0200

    Rofi theme

--- install.sh
+++ install.sh
@@ -208,8 +208,8 @@ copy_config_files() {
     cp -v $MAIN_DIR/zshrc ~/.zshrc
     cp -v $MAIN_DIR/p10k.zsh ~/.p10k.zsh
     sudo cp -v $MAIN_DIR/root-p10k.zsh /root/.p10k.zsh
-    mkdir -p ~/.config/rofi/themes
-    cp $MAIN_DIR/rofi/themes/forest.rasi ~/.config/rofi/themes/
+    # mkdir -p ~/.config/rofi/themes
+    cp -r $MAIN_DIR/rofi/* ~/.config/rofi/
     rofi-theme-selector
     cp -v $MAIN_DIR/Wallpaper/* ~/Wallpaper &>/dev/null
     echo "# WALLPAPER" >>~/.config/bspwm/bspwmrc
