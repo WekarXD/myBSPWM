82f6a3e2c59ae43806900fbc172f7969bf6ffa3c
Wekar <pablo.garcialeon@gmail.com>
2024-10-13 17:37:46 +0200

    Install v1.0.0

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Activar la salida al fallar cualquier comando
-set -e
-
 # COLORS
 RED="\e[31m"
 GREEN="\e[32m"
@@ -13,13 +10,13 @@ CYAN="\e[36m"
 WHITE="\e[97m"
 ENDCOLOR="\e[0m"
 
-# Rutas
+# Variables
 
-GITHUB_DIR=~/github
-CONFIG_DIR=~/.config
-WALLPAPER_DIR=~/Wallpapers
-ROFI_THEME_DIR=~/.config/rofi/themes
-MAIN_DIR=$(pwd)
+PAKAGE_COMMON="libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev cmake polybar bspwm rofi zsh imagemagick feh locate"
+
+GITHUB_DIR=$HOME/github
+MAIN_DIR=$HOME/myBSPWM
+CONFIG_DIR=$HOME/.config
 
 # Obtener el sistema operativo
 OS_NAME=$(grep '^NAME=' /etc/os-release | cut -d'=' -f2 | tr -d '"')
@@ -59,18 +56,6 @@ update_system() {
     sleep 3
 }
 
-# Función para verificar si un comando está instalado
-check_command() {
-    command -v "$1" >/dev/null 2>&1 || {
-        echo -e "${RED}[!] El comando '$1' no está instalado. Instalando...${ENDCOLOR}"
-        sudo $PAKAGE_MANAGER install -yqq "$1" || exit 1
-    }
-}
-
-# Lista de paquetes comunes
-PAKAGE_COMMON="libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev cmake polybar bspwm rofi zsh imagemagick feh locate"
-
-# Funcion instalar paquetes necesarios
 install_package() {
     echo -e "${BLUE}[ ] Instalando ${MAGENTA}$1...${ENDCOLOR}"
     if sudo $PAKAGE_MANAGER install -y -qq "$1" >/dev/null 2>&1; then
@@ -91,7 +76,7 @@ install_dependencies() {
     sleep 3
 
     # Comando para instalar los paquetes necesarios
-    echo -e "${YELLOW}Buscando Paquetes necesarios...${ENDCOLOR}"
+    echo -e "${BLUE}Buscando Paquetes necesarios...${ENDCOLOR}"
     for package in $PAKAGE_COMMON; do
         if ! dpkg -l | grep -q " $package "; then
             install_package "$package"
@@ -101,11 +86,11 @@ install_dependencies() {
     done
 }
 
-# Función para descargar los repositorios
 download_repositories() {
-    echo -e "${BLUE}Descargando Repositorios...${ENDCOLOR}"
+
     mkdir -p $GITHUB_DIR
     cd $GITHUB_DIR
+    echo -e "${BLUE}Descargando Repositorios...${ENDCOLOR}"
     sleep 1.5
     git clone -q https://github.com/baskerville/bspwm.git
     git clone -q https://github.com/baskerville/sxhkd.git
@@ -115,87 +100,87 @@ download_repositories() {
     echo -e "${GREEN}Repositorios descargados.${ENDCOLOR}"
 }
 
-# Función para instalar BSPWM, SXHKD, Polybar y Picom
 install_bspwm+sxhkd() {
     echo -e "${BLUE}Instalando bspwm y sxhkd...${ENDCOLOR}"
     sleep 1.5
-    cd $GITHUB_DIR/bspwm && make >/dev/null 2>&1 && sudo make install >/dev/null 2>&1
-    cd $GITHUB_DIR/sxhkd && make >/dev/null 2>&1 && sudo make install >/dev/null 2>&1
-    mkdir ~/.config/{bspwm,sxhkd}
-    cp $MAIN_DIR/Config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
-    cp $MAIN_DIR/Config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
-    chmod u+x ~/.config/bspwm/bspwmrc
-    mkdir ~/.config/bspwm/scripts
-    cp $MAIN_DIR/Config/bspwm/scripts/* ~/.config/bspwm/scripts/
-    chmod +x ~/.config/bspwm/scripts/*.sh
-    chmod +x ~/.config/bspwm/scripts/bspwm_resize
+    cd $GITHUB_DIR/bspwm && make && sudo make install
+    cd $GITHUB_DIR/sxhkd && make && sudo make install
+    mkdir $CONFIG_DIR/{bspwm,sxhkd}
+    cp $MAIN_DIR/Config/bspwm/bspwmrc $CONFIG_DIR/bspwm/bspwmrc
+    cp $MAIN_DIR/Config/sxhkd/sxhkdrc $CONFIG_DIR/sxhkd/sxhkdrc
+    chmod u+x $CONFIG_DIR/bspwm/bspwmrc
+    mkdir $CONFIG_DIR/bspwm/scripts
+    cp $MAIN_DIR/Config/bspwm/scripts/* $CONFIG_DIR/bspwm/scripts/
+    chmod +x $CONFIG_DIR/bspwm/scripts/*.sh
     echo -e "${GREEN}Listo.${ENDCOLOR}"
 }
 
 install_picom() {
-    echo -e "${BLUE}Instalando picom...${ENDCOLOR}"
+    echo -e "${BLUE}instalando picom...${ENDCOLOR}"
     sleep 1.5
     cd $GITHUB_DIR/picom && meson setup --buildtype=release build && ninja -C build && sudo ninja -C build install
     mkdir $CONFIG_DIR/picom
-    cp $MAIN_DIR/Config/picom/* $CONFIG_DIR/picom/
+    cp $MAIN_DIR/Config/picom/* $CONFIG_DIR/picom
     echo -e "${GREEN}Listo.${ENDCOLOR}"
 }
 
 install_fonts() {
-    echo -e "${BLUE}Instalando las fuentes...${ENDCOLOR}"
+    echo -e "${BLUE}Instalalndo las fuentes...${ENDCOLOR}"
     sleep 1.5
-    sudo mkdir /usr/local/share/fonts/HNF && sudo cp $MAIN_DIR/fonts/HNF/* /usr/local/share/fonts/HNF/
+    sudo mkdir /usr/local/share/fonts/HNF && sudo cp $MAIN_DIR/fonts/HNF/* /usr/local/share/fonts/
     sudo mkdir /usr/share/fonts/truetype/Polybar && sudo cp $MAIN_DIR/fonts/Polybar/* /usr/share/fonts/truetype/Polybar
     fc-cache -v
-    echo -e "${GREEN}Fuentes instaladas.${ENDCOLOR}"
 }
 
 install_kitty() {
     echo -e "${BLUE}Instalando kitty...${ENDCOLOR}"
     sleep 1.5
-    sudo mkdir -p /opt/kitty && cd /opt/kitty
+    sudo mkdir /opt/kitty && cd /opt/kitty
     kitty_last=$(curl -s -L https://github.com/kovidgoyal/kitty/releases/latest/ | grep "<title>Release v" | awk '{ print $3 }')
     sudo wget -q https://github.com/kovidgoyal/kitty/releases/download/v$kitty_last/kitty-$kitty_last-x86_64.txz
-    sudo 7z x kitty-$kitty_last-x86_64.txz && sudo rm -f kitty-$kitty_last-x86_64.txz
+    sudo 7z x kitty-$kitty_last-x86_64.txz && sudo rm -f $kitty_last-x86_64.txz
     sudo tar -xf kitty-$kitty_last-x86_64.tar && sudo rm -f kitty-$kitty_last-x86_64.tar
 
-    mkdir -p $CONFIG_DIR/kitty && sudo mkdir -p /root/.config/kitty
-    cp $MAIN_DIR/Config/kitty/kitty.conf $CONFIG_DIR/kitty/kitty.conf && sudo cp $MAIN_DIR/Config/kitty/kitty.conf /root/.config/kitty/kitty.conf
-    cp $MAIN_DIR/Config/kitty/color.ini $CONFIG_DIR/kitty/color.ini && sudo cp $MAIN_DIR/Config/kitty/color.ini /root/.config/kitty/color.ini
-    sudo ln -s -f $CONFIG_DIR/kitty/kitty.conf /root/.config/kitty/kitty.conf && sudo ln -s -f $CONFIG_DIR/kitty/color.ini /root/.config/kitty/color.ini
+    mkdir -p $CONFIG_DIR/kitty
+    sudo mkdir -p /root/.config/kitty
+    cp $MAIN_DIR/Config/kitty/kitty.conf $CONFIG_DIR/kitty/kitty.conf
+    cp $MAIN_DIR/Config/kitty/color.ini $CONFIG_DIR/kitty/color.ini
+
+    sudo cp $MAIN_DIR/Config/kitty/kitty.conf /root/.config/kitty/kitty.conf
+    sudo cp $MAIN_DIR/Config/kitty/color.ini /root/.config/kitty/color.ini
+
+    sudo ln -s -f $CONFIG_DIR/kitty/kitty.conf /root/.config/kitty/kitty.conf
+    sudo ln -s -f $CONFIG_DIR/kitty/color.ini /root/.config/kitty/color.ini
+
+    echo -e "${GREEN}LISTO.${ENDCOLOR}"
+}
+
+install_p10k() {
+    echo -e "${BLUE}Instalando P10k...${ENDCOLOR}"
+    sleep 1.5
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
+    sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/powerlevel10k
+
+    cp -rf $MAIN_DIR/Config/p10k/.p10k.zsh $HOME/.p10k.zsh
+    sudo cp -rf $MAIN_DIR/Config/p10k/.root-p10k.zsh /root/.p10k.zsh
+
     echo -e "${GREEN}Listo.${ENDCOLOR}"
 }
 
-# Función para instalar P10K
-install_p10k() {
-    echo -e "${BLUE}Instalando P10K...${ENDCOLOR}"
-    sleep 1.5
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-    sudo git clone -q --depth=1 https://github.com/romkatv/powerlevel10k.git /root/powerlevel10k
-
-    cp -rf $MAIN_DIR/Config/p10k/.p10k.zsh ~/.p10k.zsh
-    sudo cp -rf $MAIN_DIR/Config/.p10k/root-p10k.zsh /root/.p10k.zsh
-
-    echo -e "${GREEN}P10K instalado.${ENDCOLOR}"
-}
-
-# Función para configurar ZSH
 configure_zsh() {
     echo -e "${BLUE}Configurando ZSH...${ENDCOLOR}"
     sleep 1.5
-    cp -v $MAIN_DIR/Config/zsh/.zshrc ~/.zshrc && sudo cp -v $MAIN_DIR/Config/zsh/.zshrc /root/.zshrc
-    sudo ln -s -f ~/.zshrc /root/.zshrc
+    cp -v $MAIN_DIR/Config/zsh/.zshrc $HOME/.zshrc && sudo cp -v $MAIN_DIR/Config/zsh/.zshrc /root/.zshrc
+    sudo ln -s -f $HOME/.zshrc /root/.zshrc
 
     sudo chown root:root /usr/local/share/zsh/site-functions/_bspc
 
     sudo mkdir /usr/share/zsh-sudo
     sudo wget -q https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh -O /usr/share/zsh-sudo/sudo.plugin.zsh
-
     sudo usermod --shell /usr/bin/zsh root
-    echo -e "${GREEN}ZSH configurado.${ENDCOLOR}"
+    echo -e "${GREEN}Listo.${ENDCOLOR}"
 }
 
-# Función para instalar Bat, LSD y fzf
 install_custom_bins() {
     echo -e "${BLUE}Instalando binarios...${ENDCOLOR}"
     sleep 1.5
@@ -212,7 +197,7 @@ install_custom_bins() {
     sudo dpkg -i lsd_$lsd_last\_amd64.deb
 
     # Install fzf
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
     ~/.fzf/install --all
     sudo git clone -q --depth 1 https://github.com/junegunn/fzf.git /root/.fzf
     sudo /root/.fzf/install --all
@@ -220,28 +205,31 @@ install_custom_bins() {
     echo -e "${GREEN}Listo.${ENDCOLOR}"
 }
 
-# Función para copiar configuraciones
 copy_config_files() {
     echo -e "${BLUE}Copiando archivos de configuración...${ENDCOLOR}"
     sleep 1.5
 
+    #Polybar
+    cp -r -f $MAIN_DIR/Config/polybar/* $CONFIG_DIR/polybar/
+    chmod +x $CONFIG_DIR/polybar/launch.sh
+
     # Wallpaper
-    mkdir -p ~/Wallpapers
-    cp -v $MAIN_DIR/Wallpapers/* ~/Wallpapers
-    echo "# WALLPAPER" >>~/.config/bspwm/bspwmrc
-    echo "feh --bg-fill ~/Wallpapers/death.jpg &" >>~/.config/bspwm/bspwmrc
+    mkdir ~/Wallpapers
+    cp $MAIN_DIR/Wallpapers/* $HOME/Wallpapers
+    echo "# WALLPAPER" >>$CONFIG_DIR/bspwm/bspwmrc
+    echo "feh --bg-fill $HOME/Wallpapers/death.jpg &" >>$CONFIG_DIR/bspwm/bspwmrc
 
     echo -e "${GREEN}Archivos de configuración copiados.${ENDCOLOR}"
 }
 
-# Instalar Nvim + Nvchad
 install_nvim() {
-    echo -e "${BLUE}Instalando Nvim...${ENDCOLOR}"
-    cd $MAIN_DIR
+    echo -e "${GREEN}Instalando Neovim + NVChad...${ENDCOLOR}"
     sudo apt remove neovim -y
     sudo apt remove nvim -y
 
-    git clone https://github.com/NvChad/starter ~/.config/nvim
+    git clone https://github.com/NvChad/starter $CONFIG_DIR/nvim
+    sudo mkdir -p /root/.config/nvim
+    sudo cp -r $CONFIG_DIR/nvim/* /root/.config/nvim/
 
     sudo mkdir -p /opt/nvim
     cd /opt/nvim
@@ -249,33 +237,24 @@ install_nvim() {
     sudo wget -q https://github.com/neovim/neovim/releases/download/v$nvim_last/nvim-linux64.tar.gz
     sudo tar -xf nvim-linux64.tar.gz && sudo rm -f nvim-linux64.tar.gz
 
-    sudo mkdir -p /root/.config/nvim
-    sudo cp -r ~/.config/nvim/* /root/.config/nvim/
-
     # vim.opt.listchars = "tab:»·,trail:·"
     # :MasonInstallAll
     # Themes ESC + Space + th
     # Ctrl + n Lista de directorios
     # Esc + Space + ff Buscar por archivos especificos
-    # Esc + Space ch Cheatsheet de comandos
+    # Esc + Sapce ch Cheatsheet de comandos
 
     echo -e "${GREEN}Listo.${ENDCOLOR}"
 }
 
 install_rofi_themes() {
     echo -e "${BLUE}Instalando temas de Rofi...${ENDCOLOR}"
-    mkdir -p $ROFI_THEME_DIR
-    cp -r $GITHUB_DIR/rofi-themes-collection/themes/* $ROFI_THEME_DIR/
+    mkdir -p $CONFIG_DIR/rofi/themes
+    cp -r $GITHUB_DIR/rofi-themes-collection/themes/* $CONFIG_DIR/rofi/themes/
     rofi-theme-selector
     echo -e "${GREEN}Listo.${ENDCOLOR}"
 }
 
-locate() {
-    echo -e "${BLUE}Actualizando base de datos...${ENDCOLOR}"
-    #updatedb
-}
-
-# Eliminando restos
 clean() {
     echo -e "${BLUE}Limpiando restos...${ENDCOLOR}"
     rm -rf $GITHUB_DIR
@@ -283,33 +262,8 @@ clean() {
     echo -e "${GREEN}Listo.${ENDCOLOR}"
 }
 
-# Restart
-restart() {
-    notify-send -e "BSPWM Instalado
-    Hay que reiniciar y cambiar el entorno a bspwm"
-
-    while true; do
-        echo -e "${PURPLE}[?] Es necesario reiniciar el sistema. ¿Deseas reiniciar ahora?${ENDCOLOR}"
-        read -r
-        REPLY=${REPLY:-"y"}
-        if [[ $REPLY =~ ^[YySs]$ ]]; then
-            echo -e "${GREEN}[+] Reiniciando el sistema...${ENDCOLOR}"
-            sleep 1
-            sudo reboot
-        elif [[ $REPLY =~ ^[Nn]$ ]]; then
-            exit 0
-        else
-            echo -e "${RED}[!] Respuesta invalida, responda otra vez.${ENDCOLOR}"
-        fi
-    done
-}
-
-# Función principal
 main() {
     update_system
-    check_command wget
-    check_command curl
-    check_command git
     install_dependencies
     download_repositories
     install_bspwm+sxhkd
@@ -322,9 +276,25 @@ main() {
     copy_config_files
     install_nvim
     install_rofi_themes
-    locate
     clean
-    restart
 }
 
 main "$@"
+
+notify-send -e "Instalación completa
+Hay que reiniciar y cambiar el entorno a bspwm"
+
+while true; do
+    echo -e "${PURPLE}[?] Es necesario reiniciar el sistema. ¿Deseas reiniciar ahora?${ENDCOLOR}"
+    read -r
+    REPLY=${REPLY:-"y"}
+    if [[ $REPLY =~ ^[YySs]$ ]]; then
+        echo -e "${GREEN}[+] Reiniciando el sistema...${ENDCOLOR}"
+        sleep 1
+        sudo reboot
+    elif [[ $REPLY =~ ^[Nn]$ ]]; then
+        exit 0
+    else
+        echo -e "${RED}[!] Respuesta invalida, responda otra vez.${ENDCOLOR}"
+    fi
+done
